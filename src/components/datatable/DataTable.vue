04477aa84e6f6b4ce2526e7e78217c36d6203bc2
hakiiver2 <uotnnizum12@gmail.com>
2022-07-01 22:47:10 +0900

    remove reset page after sorting

--- src/components/datatable/DataTable.vue
+++ src/components/datatable/DataTable.vue
@@ -527,7 +527,7 @@ export default {
 
                         this.$emit('update:sortField', this.d_sortField);
                         this.$emit('update:sortOrder', this.d_sortOrder);
-                        this.resetPage();
+                        // this.resetPage();
                     }
                     else if (this.sortMode === 'multiple') {
                         let metaKey = event.metaKey || event.ctrlKey;
